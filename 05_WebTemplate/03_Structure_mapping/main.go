3d4c4ce35a5215e0602351617b226a31a141b38c
boseji <boseji@users.noreply.github.com>
2016-07-10 02:10:33 +0530

    Some web templating examples to show the Templating engine

---
+++ 05_WebTemplate/03_Structure_mapping/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"os"
+)
+
+type Page struct{
+	Title string
+	Body string
+}
+
+func main() {
+	tpl, err := template.ParseFiles("tpl.html")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.Execute(os.Stdout, Page{
+		Title: " This is a Title ",
+		Body: " This forms the Body ",
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println()
+}
