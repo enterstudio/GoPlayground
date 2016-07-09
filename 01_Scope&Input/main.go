7a527506b1df048cb242fec2981f81eea2fb6521
boseji <boseji@users.noreply.github.com>
2016-07-09 09:49:50 +0530

    More details for scope

--- 01_Scope&Input/main.go
+++ 01_Scope&Input/main.go
@@ -7,13 +7,22 @@ import (
 
 // checkker := true // This is wrong as the ':=' can only be used in functions
 
+func Onefunc() {
+	testvar := 35
+	fmt.Println("Printing an Internal Var: ", testvar)
+}
 
 func main() {
 	var name string
 	fmt.Printf("\n Aum Sri Ganesh !")
-	fmt.Printf("\n %#04X", submod.CheckFunction())
-	fmt.Printf("\n %#04d", submod.TestVariable)
-	fmt.Print("\n Enter Your Name: ")
+	fmt.Println("\n Checking an Imported Submodule Public Function [submod.CheckFunction()] ")
+	fmt.Printf(" %#04X\n", submod.CheckFunction())
+	fmt.Println("\n Checking an Imported Submodule Public variable [submod.TestVariable] ")
+	fmt.Printf(" %#04d\n", submod.TestVariable)
+	fmt.Println("\n Checking Input From Terminal ")
+	fmt.Print(" Enter Your Name: ")
 	fmt.Scan(&name)
-	fmt.Printf("Hello %v", name)
+	fmt.Printf("\n Hello %v\n", name)
+	fmt.Println("\n Checking Function call and internal Variable definitions ")
+	Onefunc()
 }
