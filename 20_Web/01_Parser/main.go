8293dad07a18ef4e6033ed8ff099e28707c278e2
boseji <boseji@users.noreply.github.com>
2016-07-11 23:42:06 +0530

    Template based webpages

---
+++ 20_Web/01_Parser/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"os"
+)
+
+type Page struct {
+	Title string
+	Heading string
+	Content string
+}
+func main() {
+
+	p1:= Page{
+		Title: "Go Test Page Parsing Example",
+		Heading: " Go can be really Awesome ",
+		Content: ` We can write Text or inject code
+		<script>alert("Hello");</script>`,
+	}
+
+	tmp, err:=template.ParseFiles("tpl.gohtml")
+	if err!= nil {
+		log.Println(err)
+	}
+
+	err = tmp.Execute(os.Stdout, p1)
+	if err != nil{
+		log.Println(err)
+	}
+}
