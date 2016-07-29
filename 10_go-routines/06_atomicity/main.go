7feec3a7fd547241ff311a9738c1f75a94d8c0fc
boseji <boseji@users.noreply.github.com>
2016-07-29 21:41:03 +0530

    Atomic in Go

---
+++ 10_go-routines/06_atomicity/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+	"sync/atomic"
+	"math/rand"
+)
+
+var wg sync.WaitGroup
+var counter int64
+
+func main() {
+	wg.Add(2)
+	go f1("A1")
+	go f1("A2")
+	wg.Wait()
+}
+
+func f1(s string) {
+	for i := 0; i < 10; i++ {
+		time.Sleep(time.Duration(rand.Intn(5)) * time.Millisecond)
+		atomic.AddInt64(&counter, 1)
+		fmt.Println(s, "iter:", i, "Counter:", counter)
+	}
+	wg.Done()
+}
