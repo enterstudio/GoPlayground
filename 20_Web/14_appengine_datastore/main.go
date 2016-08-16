961fce8b460cc78e76fb7f91c76e5963943d08b4
boseji <boseji@users.noreply.github.com>
2016-08-16 19:19:57 +0530

    First non-working datastore exmaple.

---
+++ 20_Web/14_appengine_datastore/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/datastore"
+	//"google.golang.org/appengine/log"
+  "golang.org/x/net/context"
+)
+
+type Entity struct {
+	Value string
+}
+
+func NumTables(ctx context.Context) (int, error) {
+	s, err := datastore.Kinds(ctx)
+	return len(s), err
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+  ctx := appengine.NewContext(r)
+
+	// Find if there are any Tables available
+	l, err := NumTables(ctx)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	fmt.Fprintf(w, " Database Available: %d", l)
+}
+
+func init() {
+	http.HandleFunc("/", handle)
+}
