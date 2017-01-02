88fb79a32f81e03e1cc33927deb3fae4197a177b
boseji <boseji@users.noreply.github.com>
2017-01-02 16:20:54 +0530

    With Logger in IRIS

---
+++ 99_Sessions/06_irisTrial/03_logger/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"gopkg.in/iris-contrib/middleware.v4/logger"
+	"gopkg.in/kataras/iris.v4"
+)
+
+func main() {
+
+	customLogger := logger.New(logger.Config{
+		// Status displays status code
+		Status: true,
+		// IP displays request's remote address
+		IP: true,
+		// Method displays the http method
+		Method: true,
+		// Path displays the request path
+		Path: true,
+	})
+
+	iris.Use(customLogger)
+
+	iris.Get("/", func(ctx *iris.Context) {
+		ctx.Write("hello")
+	})
+
+	iris.Get("/1", func(ctx *iris.Context) {
+		ctx.Write("hello")
+	})
+
+	iris.Get("/2", func(ctx *iris.Context) {
+		ctx.Write("hello")
+	})
+
+	// log http errors
+	errorLogger := logger.New()
+
+	// yes we have options look at the logger.Options inside kataras/iris/middleware/logger.go
+	iris.OnError(iris.StatusNotFound, func(ctx *iris.Context) {
+		errorLogger.Serve(ctx)
+		ctx.Write("My Custom 404 error page ")
+	})
+	//
+
+	iris.Listen(":8080")
+
+}
