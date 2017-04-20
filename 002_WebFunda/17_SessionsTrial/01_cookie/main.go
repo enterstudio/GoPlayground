7f28b3ef60844f21b2fca7d3d9166331b6f9cd8b
boseji <boseji@users.noreply.github.com>
2017-04-20 16:11:49 +0530

    Example of session cookie

---
+++ 002_WebFunda/17_SessionsTrial/01_cookie/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+func main() {
+	http.HandleFunc("/", index)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie("session")
+	if err != nil {
+		id := uuid.NewV4()
+		cookie = &http.Cookie{
+			Name:     "Session",
+			Value:    id.String(),
+			HttpOnly: true,
+			Path:     "/",
+		}
+	}
+	fmt.Println(cookie)
+	fmt.Fprint(w, cookie)
+}
