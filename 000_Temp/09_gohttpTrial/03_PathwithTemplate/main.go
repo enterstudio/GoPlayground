7670d1230b6c9ad35ed61391e6894c2bc57fe46a
boseji <prog.ic@gmail.com>
2017-09-10 07:25:25 +0000

    Adding special for C9 execution

--- 000_Temp/09_gohttpTrial/03_PathwithTemplate/main.go
+++ 000_Temp/09_gohttpTrial/03_PathwithTemplate/main.go
@@ -2,15 +2,31 @@ package main
 
 import (
 	"html/template"
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8080"
+	}
+
+	server := os.Getenv("C9_HOSTNAME")
+	if len(server) == 0 {
+		server = "0.0.0.0"
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		tpl, _ := template.ParseFiles("template/index.gohtml")
 		tpl.ExecuteTemplate(w, "index.gohtml", nil)
 	})
+
 	fs := http.FileServer(http.Dir("public"))
+
 	http.Handle("/pics/", fs)
-	http.ListenAndServe(":8080", nil)
+
+	log.Println("Running Server on " + server + ":" + port)
+	log.Fatalln(http.ListenAndServe(":"+port, nil))
 }
