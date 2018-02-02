f18a2b3197741268d4672c002f45efeab2a27530
boseji <boseji@users.noreply.github.com>
2018-02-02 19:13:46 +0530

    Adding Nested Templates

---
+++ 002_WebFunda/15_WebTemplate/05_NestedTemplate/02_nesting-with-data/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"os"
+	"text/template"
+)
+
+func main() {
+	tpl := template.Must(template.ParseGlob("*.html"))
+	tpl.ExecuteTemplate(os.Stdout, "tpl.html", "Data VAR")
+}
