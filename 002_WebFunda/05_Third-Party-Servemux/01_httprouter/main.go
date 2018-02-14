f38029c754b786ca28bb52892979a0f454df0f5e
boseji <boseji@users.noreply.github.com>
2018-02-14 18:12:26 +0530

    Adding httprouter third party mux example

---
+++ 002_WebFunda/05_Third-Party-Servemux/01_httprouter/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+}
+
+func main() {
+	router := httprouter.New()
+	router.GET("/", index)
+	router.GET("/hello/:name", hello)
+
+	log.Println("Starting Server at 8080")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	err := tpl.ExecuteTemplate(w, "index.gohtml", nil)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+}
